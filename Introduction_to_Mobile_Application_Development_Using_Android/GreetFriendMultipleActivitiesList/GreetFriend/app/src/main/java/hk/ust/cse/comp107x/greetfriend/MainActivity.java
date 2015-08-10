1e6107df65492568a618a8e94013d7e905b786c0
David Fuka <gravetotkd@gmail.com>
2015-08-09 18:13:44 -0700

    Example of Activity Lifecycle

--- Introduction_to_Mobile_Application_Development_Using_Android/GreetFriendMultipleActivitiesList/GreetFriend/app/src/main/java/hk/ust/cse/comp107x/greetfriend/MainActivity.java
+++ Introduction_to_Mobile_Application_Development_Using_Android/GreetFriendMultipleActivitiesList/GreetFriend/app/src/main/java/hk/ust/cse/comp107x/greetfriend/MainActivity.java
@@ -1,31 +1,73 @@
 package hk.ust.cse.comp107x.greetfriend;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.Calendar;
 import java.util.Date;
 
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends ListActivity {
 
-    Button greetButton;
+    String [] names;
 
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        greetButton = (Button) findViewById(R.id.greetButton);
-        greetButton.setOnClickListener(this);
+        names = getResources().getStringArray(R.array.friends);
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.friend_item, names));
+
+        Log.i(TAG, "in onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "in onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "in onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "in onRestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "in onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "in onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "in onDestroy");
     }
 
     @Override
@@ -51,31 +93,21 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
 
     @Override
-    public void onClick(View v) {
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        // create a new intent. The first parameter is the Context which is the current Activity.
+        // Hence we use "this". The second parameter is the Activity class that we wish to start.
+        // Hence it is specified as ShowMessage.class
+        Intent in = new Intent(this, ShowMessage.class);
 
-        TextView textMessage = (TextView) findViewById(R.id.textMessage);
-        EditText editFriendName = (EditText) findViewById(R.id.editFriendName);
-        String friendName = editFriendName.getText().toString();
+        // Add the message as a payload to the Intent. We add data to be carried by the intern using
+        // the putExtra() methods. The data is specified as a key-value pair. The first parameter is
+        // the key, specified as a string, and the second parameter is the value.
+        in.putExtra("message", getGreeting(getHourOfDay()) + names[(int) id] + "!");
 
-        switch (v.getId()) {
-            case R.id.greetButton:
-                // create a new intent. The first parameter is the Context which is the current Activity.
-                // Hence we use "this". The second parameter is the Activity class that we wish to start.
-                // Hence it is specified as ShowMessage.class
-                Intent in = new Intent(this, ShowMessage.class);
-
-                // Add the message as a payload to the Intent. We add data to be carried by the intern using
-                // the putExtra() methods. The data is specified as a key-value pair. The first parameter is
-                // the key, specified as a string, and the second parameter is the value.
-                in.putExtra("message", getGreeting(getHourOfDay()) + friendName + "!");
-
-                // We start the new activity by calling this method to inform the Android framework to start
-                // the new activity. The parameter is the Intent we just created earlier
-                startActivity(in);
-                break;
-            default:
-                break;
-        }
+        // We start the new activity by calling this method to inform the Android framework to start
+        // the new activity. The parameter is the Intent we just created earlier
+        startActivity(in);
     }
 
     private String getGreeting(int hour) {
